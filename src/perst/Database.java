3c2f70045e929cea5c2f0c9aaedcfa62670b4c3b
DeafBat <>
2018-06-17 15:26:12 +0200

    Comments.

--- src/perst/Database.java
+++ src/perst/Database.java
@@ -8,16 +8,21 @@ import org.garret.perst.StorageFactory;
 public class Database {
 
 	public static void main(String[] args) {
+		
 		System.out.println("--==Perst Database==--");
+		
+		//Create and open DB.
 		Storage db = StorageFactory.getInstance().createStorage();
 		db.open("test.dbs");
 		
+		//Get (or create if nonexistent) root object of DB.
 		MyRootClass root = (MyRootClass)db.getRoot();
 		if (root == null) {
 			root = new MyRootClass(db);
 			db.setRoot(root);
 		}
 		
+		//Create some objects.
 		Item sword = new Item("Sword", 200, 100);
 		Item potion = new Item("Potion", 10, 1);
 		Item cloak = new Item("Cloak", 150, 50);
@@ -33,28 +38,37 @@ public class Database {
 		map.addCharacter(monster2);
 		map.addCharacter(monster3);
 		
-		
+		//Store them.
 		root.itemIndex.put(sword);
 		root.itemIndex.put(potion);
 		root.itemIndex.put(cloak);
 		root.mapIndex.put(map);
-		
 		db.commit();
 		
+		//Close the DB.
 		db.close();
 		
+		//Open DB.
 		db.open("test.dbs");
+		
+		//Get root.
 		root = (MyRootClass)db.getRoot();
+		
+		//Get all items and run their toString method.
 		Iterator<Item> i = root.itemIndex.iterator();
 		while (i.hasNext()) {
 			Item item = (Item)i.next();
 			System.out.println(item.toString());
 		}
+		
+		//Get all maps and run their toString Methods.
 		Iterator<Map> j = root.mapIndex.iterator();
 		while (j.hasNext()) {
 			Map m = (Map)j.next();
 			System.out.println(m.toString());
 		}
+		
+		//Close the db again.
 		db.close();
 	}
 
